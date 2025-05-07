900728d3fd7c487ec78c2457a88e97eacf8138f1
witaradya <rajaradya07@gmail.com>
2025-05-07 11:55:54 +0700

    Add hamburger menu

--- js/main.js
+++ js/main.js
@@ -151,3 +151,14 @@ document.addEventListener("DOMContentLoaded", function() {
 
     animateHeader();
 });
+
+// Toggle hamburger menu on mobile
+document.addEventListener("DOMContentLoaded", function () {
+    const toggle = document.getElementById('menu-toggle');
+    const menu = document.getElementById('nav-menu');
+
+    toggle.addEventListener('click', () => {
+        menu.classList.toggle('show');
+    });
+});
+  
